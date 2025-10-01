f65266990edc43e181682fcc929ee8f32a43a425
mohitprajapati17 <mohitprajapati2444@gmail.com>
2025-10-01 19:37:21 +0530

    first microservices

---
+++ catergory-Service/src/test/java/com/mohitmac/catergory/Service/CatergoryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mohitmac.catergory.Service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CatergoryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
