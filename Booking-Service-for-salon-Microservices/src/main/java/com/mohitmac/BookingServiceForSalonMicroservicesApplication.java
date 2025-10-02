0241821d728bf013a205cdcc00c41e4e0e5ac486
mohitprajapati17 <mohitprajapati2444@gmail.com>
2025-10-02 23:05:44 +0530

    first commit

---
+++ Booking-Service-for-salon-Microservices/src/main/java/com/mohitmac/BookingServiceForSalonMicroservicesApplication.java
@@ -0,0 +1,13 @@
+package com.mohitmac;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookingServiceForSalonMicroservicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookingServiceForSalonMicroservicesApplication.class, args);
+	}
+
+}
---
+++ Booking-Service-for-salon-Microservices/src/test/java/com/mohitmac/BookingServiceForSalonMicroservicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mohitmac;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookingServiceForSalonMicroservicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
